fdca824455fed692a44515e6000fc773148becce
Michu01 <jakubmichalski01@gmail.com>
2024-06-20 21:33:14 +0200

    Add axios error handling to FormError, refactor signIn form

--- client/src/common/components/FormError.tsx
+++ client/src/common/components/FormError.tsx
@@ -1,9 +1,10 @@
+import { AxiosError } from "axios";
 import { FieldError } from "react-hook-form";
 
-function FormError({ error }: { error: FieldError | undefined }) {
+function FormError({ error }: { error: FieldError | Error | null | undefined }) {
   return (
     <div className="invalid-feedback">
-      {error?.message}
+      {(error as AxiosError<string>)?.response?.data ?? error?.message}
     </div>
   );
 }
--- client/src/signIn/Index.tsx
+++ client/src/signIn/Index.tsx
@@ -1,11 +1,24 @@
-import { FormEvent } from "react";
 import { FaAngleRight } from "react-icons/fa";
-import { AxiosError } from "axios";
 import useSignInMutation from "src/signIn/useSignInMutation";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { requiredRule } from "src/common/utility/validationRules";
+import FormError from "src/common/components/FormError";
+
+type SignInFormInputs = {
+  fullName: string;
+}
 
 function SignInIndex() {
   const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors
+    }
+  } = useForm<SignInFormInputs>();
 
   const { mutate, error, isError, isSuccess } = useSignInMutation();
 
@@ -13,15 +26,7 @@ function SignInIndex() {
     navigate(0);
   }
 
-  const message = (error as AxiosError<string>)?.response?.data;
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const fullName = e.currentTarget.fullName.value;
-
-    mutate({ fullName });
-  }
+  const onSubmit = (e: SignInFormInputs) => mutate(e);
 
   return (
     <div className="container">
@@ -29,13 +34,14 @@ function SignInIndex() {
         <div className="col-auto">
           <div className="border rounded p-5 bg-white shadow-lg" style={{ minWidth: "400px" }}>
             <h4 className="mb-3">Sign in</h4>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="form-floating mb-3">
-                <input type="text" className={`form-control ${isError && 'is-invalid'}`} id="fullName" name="fullName" placeholder="Full name" required />
+                <input 
+                  type="text" className={`form-control ${(isError || errors.fullName) && 'is-invalid'}`} id="fullName" placeholder="Full name"
+                  {...register("fullName", { required: requiredRule })}
+                />
                 <label htmlFor="fullName">Full name</label>
-                <div className="invalid-feedback">
-                  {message}
-                </div>
+                <FormError error={errors.fullName ?? error} />
               </div>
               <div className="row justify-content-center">
                 <button className="btn btn-primary col-auto d-flex align-items-center fs-6" type="submit">
