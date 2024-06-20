6e6872582cd6ca08a81ffe64ab90f4a77f1a8348
Michu01 <jakubmichalski01@gmail.com>
2024-06-20 17:52:22 +0200

    Change avatar url to depend on size

--- client/src/employees/components/EmployeeAvatar.tsx
+++ client/src/employees/components/EmployeeAvatar.tsx
@@ -1,6 +1,8 @@
+import AvatarSize from "src/common/constants/AvatarSize";
+
 function EmployeeAvatar({ employeeId, className, size }: { employeeId: number, className?: string, size: number }) {
   return (
-    <img className={className} height={size} width={size} src={`https://avatar.iran.liara.run/public?id=${employeeId}`} />
+    <img className={className} height={size} width={size} src={`https://avatar.iran.liara.run/public?size=${AvatarSize.Large}&id=${employeeId}`} />
   );
 }
 
